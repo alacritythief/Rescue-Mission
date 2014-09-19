3143da0f9cb4416d5661641fb98e5e93e77748b1
Andy Wong <alacritythief@gmail.com>
2014-09-18 21:51:30 -0400

    proper error messages show for answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,7 +3,12 @@ class AnswersController < ApplicationController
   def create
     @question = Question.find(params[:question_id])
     @answer = @question.answers.create(answer_params)
-    redirect_to question_path(@question)
+
+    if @question.save
+      redirect_to @question
+    else
+      render 'questions/show'
+    end
   end
 
   private
